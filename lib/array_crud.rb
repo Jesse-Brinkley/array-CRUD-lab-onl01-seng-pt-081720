60c6f5911eb8e09e4d8625cc9554fef9ace3bd92
Jesse Brinkley <jbrink28@gmail.com>
2020-08-21 04:51:06 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -28,7 +28,7 @@ end
 
 def retrieve_element_from_index(array, index_number)
 sodas = ["Pepsi", "Dr.Pepper", "Mountain Dew", "am"]
-sodas.index["am"]
+sodas[4]
 end
 
 def retrieve_first_element_from_array(array)
