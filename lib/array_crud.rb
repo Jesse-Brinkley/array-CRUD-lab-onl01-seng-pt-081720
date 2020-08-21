da7157366924970ed5c2c5d9030cbff2b3aac805
Jesse Brinkley <jbrink28@gmail.com>
2020-08-21 04:21:28 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -13,7 +13,7 @@ end
 
 def add_element_to_start_of_array(array, element)
   sodas = ["Pepsi", "Dr.Pepper", "Mountain Dew", "7UP"]
-  sodas >> "arrays!"
+
 end
 
 def remove_element_from_end_of_array(array)
